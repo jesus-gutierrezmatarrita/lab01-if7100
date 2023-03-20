ff12a8ebe3709b12771eda8ab12fde94dd3a4ad1
Jesús Gutiérrez Matarrita <jesusantonio.gutierrezmatarrita@gmail.com>
2023-03-19 22:05:35 -0600

    Fixed folder error

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        sayHello("Jesús");
+    }
+
+    private static void sayHello(String name) {
+        System.out.println( "Hello World! " + name );
+    }
+}
